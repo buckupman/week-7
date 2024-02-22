cf8621dbbe1fdd91f749379ae7457dbb9c03cab3
Mark Abernathy <154274762+buckupman@users.noreply.github.com>
2024-02-21 18:08:28 -0700

    Add files via upload

---
+++ mysql-java2/mysql-java2/src/main/resources/project_schema.sql
@@ -0,0 +1,56 @@
+DROP TABLE IF EXISTS project_category;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS step;
+DROP TABLE IF EXISTS material;
+DROP TABLE IF EXISTS project;
+
+CREATE TABLE project (
+	project_id INT AUTO_INCREMENT NOT NULL,
+    project_name VARCHAR(128) NOT NULL,
+    estimated_hours DECIMAL(7, 2),
+    actual_hours DECIMAL(7,2),
+    difficulty INT,
+    notes TEXT,
+    PRIMARY KEY (project_id)
+);
+
+CREATE TABLE material (
+	material_id INT AUTO_INCREMENT NOT NULL,
+    project_id INT NOT NULL,
+    num_requited INT,
+    cost DECIMAL(7, 2),
+    PRIMARY KEY (material_id),
+    FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
+);
+
+CREATE TABLE step (
+	step_id INT AUTO_INCREMENT NOT NULL,
+    project_id INT NOT NULL,
+    step_text TEXT NOT NULL,
+    step_order INT NOT NULL,
+    PRIMARY KEY (step_id),
+    FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
+);
+
+CREATE TABLE category (
+	category_id INT AUTO_INCREMENT NOT NULL,
+    category_name VARCHAR(128) NOT NULL,
+    PRIMARY KEY (category_id)
+);
+
+CREATE TABLE project_category (
+	project_id INT NOT NULL,
+    category_id INT NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES category (category_id) 
+);
+
+USE projects;
+
+INSERT INTO category (category_name) VALUES ('Carpet');
+INSERT INTO material (project_id, material_name, num_required)
+VALUES (1, '2-inch screws', 20);
+INSERT INTO step (project_id, step_text, step_order)
+VALUES (1, 'Screw door hangers on the top and bottom of each side of the door frame', 1);
+INSERT INTO project_category (project_id, category_id) VALUES (1, 2);
+    
