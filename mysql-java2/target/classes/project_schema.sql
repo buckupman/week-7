893879a7fee7181b51889246f542871c6d5324bb
Mark Abernathy <154274762+buckupman@users.noreply.github.com>
2024-02-28 18:04:07 -0700

    Add files via upload
    Week 11 coding project

--- mysql-java2/target/classes/project_schema.sql
+++ mysql-java2/target/classes/project_schema.sql
@@ -48,9 +48,7 @@ CREATE TABLE project_category (
 USE projects;
 
 INSERT INTO category (category_name) VALUES ('Carpet');
-INSERT INTO material (project_id, material_name, num_required)
-VALUES (1, '2-inch screws', 20);
-INSERT INTO step (project_id, step_text, step_order)
-VALUES (1, 'Screw door hangers on the top and bottom of each side of the door frame', 1);
+INSERT INTO material (project_id, material_name, num_required) VALUES (1, '2-inch screws', 20);
+INSERT INTO step (project_id, step_text, step_order) VALUES (1, 'Screw door hangers on the top and bottom of each side of the door frame', 1);
 INSERT INTO project_category (project_id, category_id) VALUES (1, 2);
     
